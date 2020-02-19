5d218e344617ffe3b1c51c9e1fbff5905de25bc9
Zac Clifton <zac16530@gmail.com>
2020-02-18 22:54:00 -0500

    Changed structure and added project file

--- HW/CalculationValidation3.py
+++ HW/CalculationValidation3.py
@@ -1,16 +1,17 @@
-from scipy import constants
 import math
+
+from scipy import constants
 
 h = constants.value(u'reduced Planck constant')
 pi2 = math.pow(math.pi, 2)
 
-Final = ((2 * pi2 + 3)/(2 * pi2 - 3))
+Final = ((2 * pi2 + 3) / (2 * pi2 - 3))
 
-c1 = .5 * math.sqrt((6 * pi2)/(2*pi2-3))
+c1 = .5 * math.sqrt((6 * pi2) / (2 * pi2 - 3))
 
 
 def cn(nput):
-    t1 = (1/pi2)
+    t1 = (1 / pi2)
     t2 = math.sqrt((24 * pi2) / (2*pi2-3))
     t3 = ((1/math.pow(nput-1, 2)) - (1/math.pow(nput+1, 2)))
     temp = math.pow(t1 * t2 * t3, 2) * math.pow(nput, 2)
@@ -19,7 +20,7 @@ def cn(nput):
 
 print("Final answer should be: " + str(Final))
 
-ans = c1
+ans = c1 ** 2
 for i in range(2, 20, 2):
     ans += cn(i)
 
